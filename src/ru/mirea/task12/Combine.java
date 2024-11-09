be17e2bc6e7b97a150e85436ae6e770168cb46c4
egor <mezegal@yandex.ru>
2024-11-09 23:44:28 +0300

    14 all done

--- src/ru/mirea/task12/Combine.java
+++ src/ru/mirea/task12/Combine.java
@@ -14,7 +14,7 @@ public class Combine extends JFrame {
         Font fnt = new Font("Times new roman",Font.BOLD,20);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel[] pnl = new JPanel[3];
+        JPanel[] pnl = new JPanel[4];
         frame.setLayout(new GridLayout(2,2));
         for(int i = 0 ; i < pnl.length ; i++){
             int r = (int) (Math.random() * 255);
@@ -24,6 +24,7 @@ public class Combine extends JFrame {
             pnl[i].setBackground(new Color(r,g,b));
             frame.add(pnl[i]);
         }
+        pnl[3].setLayout(new GridLayout(1,2));
 
         JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu("File");
@@ -34,7 +35,16 @@ public class Combine extends JFrame {
                 System.exit(0);
             }
         });
+        JMenuItem restartItem = new JMenuItem("Перезапуск программы");
+        restartItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new Combine();
+            }
+        });
         menu.add(exitItem);
+        menu.add(restartItem);
         menuBar.add(menu);
         frame.setJMenuBar(menuBar);
         JButton jbutton = new JButton("Clear");
@@ -44,10 +54,27 @@ public class Combine extends JFrame {
                 jta.setText("");
             }
         });
+        JButton exitButton = new JButton("EXIT");
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        JButton restartButton = new JButton("Restart");
+        restartButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new Combine();
+            }
+        });
         pnl[0].add(new Animation());
         pnl[1].add(new Match());
         pnl[2].add(jta);
         pnl[2].add(jbutton);
+        pnl[3].add(exitButton);
+        pnl[3].add(restartButton);
         jta.setForeground(Color.GREEN);
         jta.setFont(fnt);
         frame.pack();
